20b8faa4a1747d2b6ed9cea327797f672019193e
victorshehu <44091812+victorshehu@users.noreply.github.com>
2019-11-20 16:44:23 -0500

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-      $host = "mysql3.hostica.com";
-      $user = "victors_myuser";
-      $pass = "TestFor2!";
-      $db   = "victors_Nursingtest";
-  
-
-$conn = mysqli_connect ($host, $user, $pass, $db);
-?>
