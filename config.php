449c68124d08f4aba6a978d1bc36312066537815
victorshehu <44091812+victorshehu@users.noreply.github.com>
2019-11-20 12:41:21 -0500

    login and css
    login for database and css for result page

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+      $host = "mysql3.hostica.com";
+      $user = "victors_myuser";
+      $pass = "TestFor2!";
+      $db   = "victors_Nursingtest";
+  
+
+$conn = mysqli_connect ($host, $user, $pass, $db);
+?>
